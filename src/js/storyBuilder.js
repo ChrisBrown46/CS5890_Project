69e8a3fd3d743326ceda47e2b3aa8f40d2b47962
Christopher Brown <christopher.r.brown@aggiemail.usu.edu>
2018-11-15 21:06:28 -0700

    Battle navigation chart (#17)
    * Added a prototype of the battle visualization chart
    
    Signed-off-by: ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
    
    * Changed bootstrap scrolling to Waypoint for better control over triggers
    
    Signed-off-by: ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
    
    * Update storyBuilder.js
    
    * Waypoint triggers (#16)
    
    * Adding battles json
    
    Signed-off-by: Justin Fairbourn <jfairbourn96@bitbucket.org>
    
    * Finished with all descriptions except for the Conclusion.
    
    * Battle navigation chart (#14)
    
    * Added a prototype of the battle visualization chart
    
    Signed-off-by: ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
    
    * Changed bootstrap scrolling to Waypoint for better control over triggers
    
    Signed-off-by: ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
    
    * Update storyBuilder.js
    
    * Create triggers for each datum in the battle text
    
    Signed-off-by: ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>

--- src/js/storyBuilder.js
+++ src/js/storyBuilder.js
@@ -1,28 +1,27 @@
-function buildStory() {
- d3.json("/src/resources/battle-text/description.json").then(function(data) {
-    console.log(data);
+function buildStory(battleNavigationChart) {
+ d3.json("/src/resources/battle-text.json").then(function(data) {
+    const text = d3
+      .select("#story")
+      .selectAll("div")
+      .data(data)
+      .enter()
+      .append("div")
+      .attr("id", (_, i) => "battle-" + (i + 1));
+
+    text
+      .append("p")
+      .attr("class", "h1")
+      .text(d => d.battle);
+
+    text
+      .selectAll("p")
+      .data(d => d.text)
+      .enter()
+      .append("p")
+      .attr("class", "h3")
+      .attr("id", function(_, i) { return this.parentNode.id + "-" + i; })
+      .text(d => d);
+
+    buildWaypoints(battleNavigationChart);
   });
-
-  const data = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tristique turpis maximus neque pellentesque rhoncus. Suspendisse at vestibulum eros, vel auctor eros. Praesent fringilla consectetur odio non suscipit. Mauris et luctus dui. Nunc et imperdiet augue. Nam id lorem et felis laoreet pretium. In nec risus et nibh ultrices hendrerit eget non velit. Cras eros dui, hendrerit non lobortis ut, tristique id dolor. Quisque turpis nunc, ultrices eu fermentum ultrices, pulvinar eu mi. Etiam ultricies, nulla a malesuada tempor, sapien sapien auctor velit, sed lobortis nunc dui in nunc. Suspendisse ac varius nisl. Fusce viverra sapien id iaculis laoreet. Integer a felis id metus convallis consequat eu ac sapien.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tristique turpis maximus neque pellentesque rhoncus. Suspendisse at vestibulum eros, vel auctor eros. Praesent fringilla consectetur odio non suscipit. Mauris et luctus dui. Nunc et imperdiet augue. Nam id lorem et felis laoreet pretium. In nec risus et nibh ultrices hendrerit eget non velit. Cras eros dui, hendrerit non lobortis ut, tristique id dolor. Quisque turpis nunc, ultrices eu fermentum ultrices, pulvinar eu mi. Etiam ultricies, nulla a malesuada tempor, sapien sapien auctor velit, sed lobortis nunc dui in nunc. Suspendisse ac varius nisl. Fusce viverra sapien id iaculis laoreet. Integer a felis id metus convallis consequat eu ac sapien.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tristique turpis maximus neque pellentesque rhoncus. Suspendisse at vestibulum eros, vel auctor eros. Praesent fringilla consectetur odio non suscipit. Mauris et luctus dui. Nunc et imperdiet augue. Nam id lorem et felis laoreet pretium. In nec risus et nibh ultrices hendrerit eget non velit. Cras eros dui, hendrerit non lobortis ut, tristique id dolor. Quisque turpis nunc, ultrices eu fermentum ultrices, pulvinar eu mi. Etiam ultricies, nulla a malesuada tempor, sapien sapien auctor velit, sed lobortis nunc dui in nunc. Suspendisse ac varius nisl. Fusce viverra sapien id iaculis laoreet. Integer a felis id metus convallis consequat eu ac sapien.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tristique turpis maximus neque pellentesque rhoncus. Suspendisse at vestibulum eros, vel auctor eros. Praesent fringilla consectetur odio non suscipit. Mauris et luctus dui. Nunc et imperdiet augue. Nam id lorem et felis laoreet pretium. In nec risus et nibh ultrices hendrerit eget non velit. Cras eros dui, hendrerit non lobortis ut, tristique id dolor. Quisque turpis nunc, ultrices eu fermentum ultrices, pulvinar eu mi. Etiam ultricies, nulla a malesuada tempor, sapien sapien auctor velit, sed lobortis nunc dui in nunc. Suspendisse ac varius nisl. Fusce viverra sapien id iaculis laoreet. Integer a felis id metus convallis consequat eu ac sapien.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tristique turpis maximus neque pellentesque rhoncus. Suspendisse at vestibulum eros, vel auctor eros. Praesent fringilla consectetur odio non suscipit. Mauris et luctus dui. Nunc et imperdiet augue. Nam id lorem et felis laoreet pretium. In nec risus et nibh ultrices hendrerit eget non velit. Cras eros dui, hendrerit non lobortis ut, tristique id dolor. Quisque turpis nunc, ultrices eu fermentum ultrices, pulvinar eu mi. Etiam ultricies, nulla a malesuada tempor, sapien sapien auctor velit, sed lobortis nunc dui in nunc. Suspendisse ac varius nisl. Fusce viverra sapien id iaculis laoreet. Integer a felis id metus convallis consequat eu ac sapien.",
-  ];
-
-  const text = d3.select("#story")
-    .selectAll("div")
-    .data(data)
-    .enter()
-    .append("div")
-    .attr("id", (_, i) => "battle-" + (i + 1));
-
-  text.append("p")
-    .attr("class", "h1")
-    .text((_, i) => "Battle " + (i + 1))
-
-  text.append("p")
-    .attr("class", "h3")
-    .text(d => d);
 }
