4d419cc9e0299f3fce3d0feea44be8c1939b7603
ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
2018-11-21 14:10:02 -0700

    Improved popup load time and battle switching
    Signed-off-by: ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,12 @@ d3.json("resources/battle-data/battle-data.json").then(battleData => {
 
   const battleNavigationChart = new BattleNavigationChart(battleData);
   const worldMap = new WorldMap(battleData);
+  const popupChart = new PopupChart(battleData);
 
   d3.json("resources/world.json")
     .then(function(world) {
       worldMap.drawMap(world);
     });
 
-  buildStory(battleNavigationChart, worldMap);
+  buildStory(battleNavigationChart, worldMap, popupChart);
 });
--- src/js/waypointBuilder.js
+++ src/js/waypointBuilder.js
@@ -1,4 +1,4 @@
-function buildBattle(battleNavigationChart, battleNumber, paragraphCount) {
+function buildBattle(battleNavigationChart, worldMap, popupChart, battleNumber, paragraphCount) {
   const halfway = Math.trunc(paragraphCount / 2);
 
   for (let index = 1; index <= paragraphCount; index += halfway) {
@@ -7,7 +7,9 @@ function buildBattle(battleNavigationChart, battleNumber, paragraphCount) {
     new Waypoint({
       element: document.getElementById("battle-" + battleNumber + "-" + index),
       handler: function(direction) {
-        battleNavigationChart.update(battleNumber, percent, direction)
+        battleNavigationChart.update(battleNumber, percent, direction);
+        worldMap.update(battleNumber, percent, direction);
+        popupChart.update(battleNumber, percent, direction);
       },
       context: document.getElementById("story"),
       offset: "50%"
@@ -15,11 +17,13 @@ function buildBattle(battleNavigationChart, battleNumber, paragraphCount) {
   }
 }
 
-function buildConclusion(battleNavigationChart) {
+function buildConclusion(battleNavigationChart, worldMap, popupChart) {
   new Waypoint({
     element: document.getElementById("battle-12-1"),
     handler: function() {
-      battleNavigationChart.update(12, 1, "down")
+      battleNavigationChart.update(12, 1, "down");
+      worldMap.update(12, 1, "down");
+      popupChart.update(12, 1, "down");
     },
     context: document.getElementById("story"),
     offset: "50%"
@@ -28,24 +32,24 @@ function buildConclusion(battleNavigationChart) {
   new Waypoint({
     element: document.getElementById("battle-12-4"),
     handler: function() {
-      battleNavigationChart.update(13, 1, "down")
+      battleNavigationChart.update(13, 1, "down");
     },
     context: document.getElementById("story"),
     offset: "95%"
   });
 }
 
-function buildWaypoints(battleNavigationChart) {
-  buildBattle(battleNavigationChart, 1, 4);
-  buildBattle(battleNavigationChart, 2, 4);
-  buildBattle(battleNavigationChart, 3, 2);
-  buildBattle(battleNavigationChart, 4, 2);
-  buildBattle(battleNavigationChart, 5, 4);
-  buildBattle(battleNavigationChart, 6, 4);
-  buildBattle(battleNavigationChart, 7, 4);
-  buildBattle(battleNavigationChart, 8, 2);
-  buildBattle(battleNavigationChart, 9, 4);
-  buildBattle(battleNavigationChart, 10, 4);
-  buildBattle(battleNavigationChart, 11, 4);
-  buildConclusion(battleNavigationChart);
+function buildWaypoints(battleNavigationChart, worldMap, popupChart) {
+  buildBattle(battleNavigationChart, worldMap, popupChart, 1, 4);
+  buildBattle(battleNavigationChart, worldMap, popupChart, 2, 4);
+  buildBattle(battleNavigationChart, worldMap, popupChart, 3, 2);
+  buildBattle(battleNavigationChart, worldMap, popupChart, 4, 2);
+  buildBattle(battleNavigationChart, worldMap, popupChart, 5, 4);
+  buildBattle(battleNavigationChart, worldMap, popupChart, 6, 4);
+  buildBattle(battleNavigationChart, worldMap, popupChart, 7, 4);
+  buildBattle(battleNavigationChart, worldMap, popupChart, 8, 2);
+  buildBattle(battleNavigationChart, worldMap, popupChart, 9, 4);
+  buildBattle(battleNavigationChart, worldMap, popupChart, 10, 4);
+  buildBattle(battleNavigationChart, worldMap, popupChart, 11, 4);
+  buildConclusion(battleNavigationChart, worldMap, popupChart);
 }
