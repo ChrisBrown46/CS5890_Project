02d8865f7eb373fad734a48cc2389701303422f8
Christopher Brown <christopher.r.brown@aggiemail.usu.edu>
2018-11-28 18:37:24 -0700

    World map color zooming (#27)
    * Added color and styling to the map
    
    * Rescaled map and remove popup
    
    Signed-off-by: ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>

--- src/js/worldMap.js
+++ src/js/worldMap.js
@@ -19,7 +19,24 @@ class WorldMap {
       .attr("height", this.svgHeight)
       .attr("id", "map-chart")
       .append("g")
-      .attr("transform", "translate(50, 50)");
+      .attr("transform", "translate(5, 150) scale(1.395, 1.395)");
+  }
+
+  updateSides() {
+    const allies_iso = ["AUS", "BRA", "CAN", "NZL", "ZAF", "RUS", "GBR", "USA"]
+    const axis_iso = ["DEU", "ITA", "JPN", "BGR", "HUN", "ROU", "SVK", "AUT", "ETH", "CHN"]
+
+    allies_iso.forEach(allies_iso => {
+      this.svg
+        .selectAll("path#" + allies_iso)
+        .classed("side-allies", true);
+    })
+
+    axis_iso.forEach(axis_iso => {
+      this.svg
+        .selectAll("path#" + axis_iso)
+        .classed("side-axis", true);
+    })
   }
 
   updateSides() {
