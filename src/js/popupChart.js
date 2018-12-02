3c170cff9c6afd8186ba1847752d3b6ea59cc1e0
Justin Fairbourn <jfairbourn96@bitbucket.org>
2018-12-02 04:26:20 -0700

    Got bars and everything else working great. Just need to fix axes.

--- src/js/popupChart.js
+++ src/js/popupChart.js
@@ -54,10 +54,12 @@ class PopupChart {
     // this.currentBattle = this.battleData[0];
     this.chartDiv = d3.select(`div#chartDiv${chartNumber}`);
     this.dimensions = this.chartDiv.node().getBoundingClientRect();
+    this.height = parseFloat(this.dimensions.height);
+    this.width = parseFloat(this.dimensions.width);
     this.selectBox = this.chartDiv.append("select").node();
     this.selectBox.onchange = () => this.updateBars();
     
-    // this.selectBox.hidden = true;
+    this.selectBox.hidden = true;
 
     this.chartSvg = this.chartDiv.append("svg")
       .attr("id", `chart${chartNumber}`)
@@ -65,7 +67,7 @@ class PopupChart {
       .attr("height", "100%");
 
     this.popup = this.chartSvg.append("g")
-      .attr("transform", `scale(1, -1) translate(0, -${this.dimensions.height})`);
+      .attr("transform", `scale(1, -1) translate(0, -${this.height})`);
 
     for (let option of options){
       let newOption = document.createElement("option");
@@ -77,26 +79,26 @@ class PopupChart {
     // Initialize the Y-axis line
     this.popupY = this.popup
       .append("line")
-      .attr("x1", this.dimensions.width * 0.20)
-      .attr("x2", this.dimensions.width * 0.20)
-      .attr("y1", this.dimensions.height * 0.85)
-      .attr("y2", this.dimensions.height * 0.15)
+      .attr("x1", this.width * 0.20)
+      .attr("x2", this.width * 0.20)
+      .attr("y2", this.height * 0.85)
+      .attr("y1", this.height * 0.15)
       .attr("class", "axis-line");
 
     // Initialize the X-axis line
     this.popupX = this.popup
       .append("line")
-      .attr("x1", this.dimensions.width * 0.20)
-      .attr("x2", this.dimensions.width * 0.85)
-      .attr("y1", this.dimensions.height * 0.15)
-      .attr("y2", this.dimensions.height * 0.15)
+      .attr("x1", this.width * 0.20)
+      .attr("x2", this.width * 0.95)
+      .attr("y1", this.height * 0.15)
+      .attr("y2", this.height * 0.15)
       .attr("class", "axis-line");
 
     // Initialize the "no data found" text
     this.noDataText = this.popup.append("text")
       .text("No data found!")
-      .attr("x", this.dimensions.width * 0.2)
-      .attr("y", this.dimensions.height * -0.6)
+      .attr("x", this.width * 0.2)
+      .attr("y", this.height * -0.6)
       .attr("transform", "scale(1,-1)")
       .attr("hidden", "true");
 
@@ -115,12 +117,57 @@ class PopupChart {
     this.currentAttribute = this.selectBox.options[this.selectBox.selectedIndex].value;
 
     // Then see if there is data for this category for the currently selected battle and behave accordingly:
-    if(this.currentBattle.categories[this.currentAttribute] != undefined){
-      console.log("found");
+
+    let categoryDomain = this.currentBattle.categories[this.currentAttribute];
+    if(categoryDomain != undefined){
       this.popupX.attr("hidden", null);
       this.popupY.attr("hidden", null);
       this.noDataText.attr("hidden", "true");
+      this.popup.selectAll("rect").remove();
+
+      let chartData = [];
+
+      for (const force in this.currentBattle.forces_data) {
+        let newVal = this.currentBattle.forces_data[force][this.currentAttribute];
+        if (newVal === undefined) newVal = 0;
+  
+        chartData.push({
+          name: force,
+          val: newVal
+        });
+      }
+      
+      const yScale = d3.scaleLinear()
+        .domain([0, categoryDomain])
+        .range([0, this.height * 0.7]);
+
+      let barWidth = this.width * 0.75 / chartData.length;
+
+      this.popup
+        .selectAll("rect")
+        .data(chartData)
+        .enter()
+        .append("rect")
+        .attr("x", (_, i) => i * barWidth + this.width * 0.2)
+        .attr("y", this.height * 0.15)
+        .attr("val", d => d.val)
+        .attr("height", d => yScale(d.val))
+        .attr("class", d => `bar${d.name}`)
+        .attr("width", () => barWidth - 3);
+      
+      this.popup
+        .selectAll("text.label")
+        .data(chartData)
+        .enter()
+        .append("text")
+        .text(d => d.name)
+        .attr("x", (_, i) => i * barWidth + this.width * 0.2 + 2)
+        .attr("y", this.height * -0.1)
+        .attr("transform", "scale(1, -1)")
+        .style("color", "white")
+        .attr("class", "label")
     }
+
     else {
       // Data Not found for this attribute in this battle
       this.popup.selectAll("text.label").remove();
@@ -137,85 +184,4 @@ class PopupChart {
       else option.disabled = false;
     }
   }
-  
-  // Creates the dropdown menu that allows the user to select which category they want to view
-  // trigger this each time the scrollytelling changes to a new visualization
-  buildPopup(battleJson) {
-    console.log(this.dimensions());
-    this.currentBattle = battleJson;
-    this.configureOptions();
-    // Re-initialize the category selectbox
-    this.popup
-      .selectAll("*")
-      .remove();
-
-    
-    // Add the axis lines to the chart
-    
-    // // Initialize the bar chart
-    // this.buildChart(
-    //   categoryMenu.value, 
-    //   battleJson.forces_data, 
-    //   categories[categoryMenu.value]
-    // );
-  
-    // // Set the select's onchange to dynamically load new data
-
-  }
-
-  // Change the svg according to the specified category
-  buildChart(category, forces, dataDomain) {
-    const chartData = [];
-
-    // Parse out and validate the data from battleJson
-    for (const force in forces) {
-      let newVal = forces[force][category];
-      if (newVal === undefined) newVal = 0;
-
-      chartData.push({
-        name: force,
-        val: newVal
-      });
-    }
-
-    // // Clear the old data
-    // this.popup.selectAll("text").remove();
-    // this.popup.selectAll("rect").remove();
-
-    // if (category === "none") {
-    //   // Output a message if "none" is selected
-    //   this.popup
-    //     .append("text")
-    //     .text("Click on a category!")
-    //     .attr("x", 100)
-    //     .attr("y", 100);
-    // } else {
-    //   // Establish the yScale used for the y-axis
-    //   const yScale = d3.scaleLinear()
-    //     .domain([0, dataDomain])
-    //     .range([245, 10]);
-
-    //   // Add one rectangle for each data entry
-    //   this.popup
-    //     .selectAll("rect")
-    //     .data(chartData)
-    //     .enter()
-    //     .append("rect")
-    //     .attr("x", (_, i) => i * (250 / chartData.length) + 55)
-    //     .attr("y", d => yScale(d.val))
-    //     .attr("val", d => d.val)
-    //     .attr("height", d => 245 - yScale(d.val))
-    //     .attr("class", d => `bar ${d.name}`)
-    //     .attr("width", () => 250 / chartData.length - 3)
-    // }
-  }
-
-  // update(battleNumber, textPosition, direction) {
-    // if (textPosition !== 1) return;
-
-    // if (direction === "up")
-    //   battleNumber -= 1;
-    
-    // this.buildPopup(this.battleData[battleNumber - 1]);
-  // }
 }
